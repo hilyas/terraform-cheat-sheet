64400d26e42260f0b65712d562e267144609b611
hilyas <hilyas@users.noreply.github.com>
2023-08-28 19:53:46 +0100

    rename some resources

--- aws_vpc/dependencies.tf
+++ aws_vpc/dependencies.tf
@@ -2,20 +2,20 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_vpc" "ltthw-vpc" {
+resource "aws_vpc" "my-vpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
-        Name = "ltthw-vpc"
+        Name = "my-vpc"
     }
 }
 
-resource "aws_subnet" "ltthw-vpc-subnet" {
-    vpc_id = "${aws_vpc.ltthw-vpc.id}"
-    cidr_block = "${aws_vpc.ltthw-vpc.cidr_block}"
+resource "aws_subnet" "my-vpc-subnet" {
+    vpc_id = "${aws_vpc.my-vpc.id}"
+    cidr_block = "${aws_vpc.my-vpc.cidr_block}"
 }
 
 resource "local_file" "local-file" {
     filename = "${path.module}/local_file.txt"
     content = "This is a local file"
-    depends_on = [aws_vpc.ltthw-vpc]
+    depends_on = [aws_vpc.my-vpc]
 }
